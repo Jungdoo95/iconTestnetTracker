90c4119c514f7db6a322d0d168eb4441aa6aaae4
ddudoo95@gmail.com <ddudoo95@gmail.com>
2019-06-12 06:13:54 +0000

    add wallet app

--- iconTestnetTracker/settings.py
+++ iconTestnetTracker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tracker.apps.TrackerConfig',
+    'walletApp.apps.WalletappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
